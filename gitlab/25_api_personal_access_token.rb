ded698d5334f77d97e03944f5e7d85053a0d6fb5
Vincent Lussenburg <vincent@lussenburg.net>
2023-06-27 12:04:01 -0700

    PAT script fix for newer gitlab versions

--- gitlab/25_api_personal_access_token.rb
+++ gitlab/25_api_personal_access_token.rb
@@ -7,11 +7,23 @@ puts "Creating api access token for root user".color(:red)
 puts "---------------------------------------".color(:red)
 puts "=======================================".color(:red)
 
-token = PersonalAccessToken.new
-token.user_id = User.find_by(username: 'root').id
-token.name = 'api-token-for-testing'
-token.scopes = ["api"]
-token.set_token(ENV['GITLAB_ROOT_PASSWORD'])
-token.save
 
-print 'OK'
+# Create an api access token for root user with the value:
+scopes = Gitlab::Auth.all_available_scopes
+
+Gitlab::Seeder.quiet do
+  User.find_by(username: 'root').tap do |user|
+    params = {
+      scopes: scopes.map(&:to_s),
+      name: 'seeded-api-token'
+    }
+
+    user.personal_access_tokens.build(params).tap do |pat|
+      pat.expires_at = 365.days.from_now
+      pat.set_token(ENV['GITLAB_ROOT_PASSWORD'])
+      pat.save!
+    end
+  end
+
+  print 'OK'
+end
